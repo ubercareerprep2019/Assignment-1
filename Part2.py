200aef885e21422c4a54063b2bf16703acd05f40
Younis Ereiqat <46542510+yuunji@users.noreply.github.com>
2019-05-25 13:37:48 -0700

    Update Part2.py

--- Part2.py
+++ Part2.py
@@ -1,1 +1,24 @@
+def isStringPermutation(s1, s2): #assuming strings are the same length
+    count = 0 
+    
+    for char in s1:
+        
+        if char in s2:
+            count += 1
 
+    return count == len(s1) 
+    
+    
+
+
+def pairsThatEqualSum(intlist, targ):
+    targlist = []
+    
+    for num1 in intlist: #checks all possible sum pairs in nested for loop
+    
+        for num2 in intlist:
+            
+            if num1 + num2 == targ and targlist.count((num1,num2)) == 0: #no duplicates
+                targlist.append((num1, num2))
+                
+    return targlist
