2d5febf5cfd46f2fd7ae886d3b5f10ee779e432f
Younis Ereiqat <46542510+yuunji@users.noreply.github.com>
2019-06-20 12:45:05 -0700

    Update Part5.py

--- Part5.py
+++ Part5.py
@@ -1,1 +1,9 @@
-
+class TowerOfHanoi
+    def __init__(self, n):
+        self.RodA = [i for i in range(n, 0, -1)]
+        self.RodB = []
+        self.RodC = []
+        self.n = n
+        self.moves = 0
+        self.RodCount = 3
+        
